30b6631a1a389dff59692de98191ee427d2bef10
samuel <samuel2002espinoza@gmail.com>
2023-08-07 12:07:39 -0600

    setup for testing, it's divide in groups the testing code

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,22 +1,30 @@
 const { sum, multiply, divide } = require('./02-math');
 
-test('add 1 + 3 should be 4', () => {
-  const rsa = sum(1, 3);
-  expect(rsa).toBe(4);
-});
-test('shuld be 4', () => {
-  const rta = multiply(1, 4);
-  expect(rta).toBe(4);
-});
-test('shuld divide', () => {
-  const rta = divide(6, 3);
-  expect(rta).toBe(2);
-  const rta2 = divide(5, 2);
-  expect(rta2).toBe(2.5);
-});
-test('shold divide for zero', () => {
-  const rta = divide(6, 3);
-  expect(rta).toBe(2);
-  const rta2 = divide(9, 0);
-  expect(rta2).toBe(null);
+describe('Test for math', () => {
+  describe('test for sum ', () => {
+    test('add 1 + 3 should be 4', () => {
+      const rsa = sum(1, 3);
+      expect(rsa).toBe(4);
+    });
+  });
+  describe('test for multiply ', () => {
+    test('shuld be 4', () => {
+      const rta = multiply(1, 4);
+      expect(rta).toBe(4);
+    });
+  });
+  describe('test for divide ', () => {
+    test('shuld divide', () => {
+      const rta = divide(6, 3);
+      expect(rta).toBe(2);
+      const rta2 = divide(5, 2);
+      expect(rta2).toBe(2.5);
+    });
+    test('shold divide for zero', () => {
+      const rta = divide(6, 3);
+      expect(rta).toBe(2);
+      const rta2 = divide(9, 0);
+      expect(rta2).toBe(null);
+    });
+  });
 });
--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -1,13 +1,19 @@
 const Person = require('./06-person');
 
-test('should return down', () => {
-  const person = new Person('Nicolas', 45, 1.7);
-  const imc = person.calcIMC();
-  expect(imc).toBe('down');
-})
+describe('Test for Person', () => {
+  let person;
+  beforeEach(() => {
+    person = new Person('Nicolas', 45, 1.7);
+  });
+  test('should return down', () => {
+    person.weight = 45;
+    const imc = person.calcIMC();
+    expect(imc).toBe('down');
+  });
 
-test('should return normal', () => {
-  const person = new Person('Nicolas', 59, 1.7);
-  const imc = person.calcIMC();
-  expect(imc).toBe('normal');
-})
+  test('should return normal', () => {
+    person.weight = 59;
+    const imc = person.calcIMC();
+    expect(imc).toBe('normal');
+  });
+});
